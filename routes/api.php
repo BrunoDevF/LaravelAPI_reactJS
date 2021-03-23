227df94357322e7d07ce9241dbafc4570cb5542c
Bruno Henrique <brunohenriquex00@gmail.com>
2021-03-22 22:48:59 -0300

    editando variavel da rota upload

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('upload', function (Files $files, Request $request) {
+    // $file = $request->files->get('file');
+    
+    // $files->name = $file->getClientOriginalName();
+    // $files->size = $file->getSize();
+    // $files->type = $file->getClientOriginalExtension();
+    // $files->url = $request->file->store('drop');
+    // $files->save();
+    
+    //TESTE COM PASTAS 
     $file = $request->files->get('file');
     
     $files->name = $file->getClientOriginalName();
     $files->size = $file->getSize();
     $files->type = $file->getClientOriginalExtension();
-    $files->url = $request->file->store('drop');
+
+    if(!empty($request->input('folder'))){
+        $folder = $request->folder;
+        //dd($pasta);
+        $files->url = $request->file->store($folder);
+    }else{
+        $files->url = $request->file->store('drop');
+    }
     $files->save();
 
     return [
