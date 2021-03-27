6e31204ce18bc6ad1d74007785b4be14585030fd
Bruno Henrique <brunohenriquex00@gmail.com>
2021-03-27 16:22:17 -0300

    Projeto ReactJS incluido

--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('dir', function(){
+    $removeFoldersInPath = function(string $path) {
+        $parts = explode("/", $path);
+        return end($parts);
+    };
+    $directories = array_reduce(
+        Storage::allDirectories('./'),
+        function(array $all, string $directory) use ($removeFoldersInPath) {
+            $all[$directory] = array_map(
+                $removeFoldersInPath,
+                Storage::allFiles($directory)
+            );
+            return $all;
+        },
+        []
+    );
+    return $directories;
+
+});
+
 Route::post('upload', function (Files $files, Request $request) {
-    // $file = $request->files->get('file');
     
-    // $files->name = $file->getClientOriginalName();
-    // $files->size = $file->getSize();
-    // $files->type = $file->getClientOriginalExtension();
-    // $files->url = $request->file->store('drop');
-    // $files->save();
-    
-    //TESTE COM PASTAS 
+
+    $request->validate([
+        'files' => 'require|image|pdf|zip'
+    ]);
     $file = $request->files->get('file');
     
     $files->name = $file->getClientOriginalName();
@@ -29,9 +45,9 @@ Route::post('upload', function (Files $files, Request $request) {
     if(!empty($request->input('folder'))){
         $folder = $request->folder;
         //dd($pasta);
-        $files->url = $request->file->store($folder);
+        $files->url = $request->file->storeAs($folder, $file->getClientOriginalName());
     }else{
-        $files->url = $request->file->store('drop');
+        $files->url = $request->file->storeAs('drop', $file->getClientOriginalName());
     }
     $files->save();
 
@@ -51,8 +67,9 @@ Route::get('list', function(Request $request, Files $files){
 
     //caso queira corrigir url da imagem 
     //pode ser rodado um foreach pegando a url no storage 
-    //foreach ($files as $key => $value) {
-        //$files[$key]['url'] = asset($files['url']);
-    //}
+    // foreach ($files as $key => $value) {
+    //     $files[$key]['url'] = asset($files['url']);
+    // }
     return $files;
 });
+
